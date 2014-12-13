68bd915ec097d433290dce43c51b8e88542c20fe
Łukasz Biedrycki <lukasz.biedrycki@gmail.com>
2014-12-13 16:58:11 +0000

    Do not crash poolboy worker when no connection

--- src/erlcql_cluster_worker.erl
+++ src/erlcql_cluster_worker.erl
@@ -35,7 +35,12 @@ handle_cast(Request, State) ->
 
 handle_info(timeout, #state{client = Pid} = State) ->
     %% There's no ping, so send the options request
-    {ok, _} = erlcql_client:options(Pid),
+    _ = case erlcql_client:options(Pid) of
+            {ok, _} = Ok -> Ok;
+            {error, _} = Error ->
+                % warning, not connected
+                Error
+        end,
     {noreply, State, ?KEEPALIVE};
 handle_info(Info, State) ->
     {stop, {bad_info, Info}, State}.
